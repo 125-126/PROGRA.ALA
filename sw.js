9bc752358ad5b830f810740748f888ba141cf135
CARLOS ALEJANDRO GARCIA <carlosale.miranda555@gmail.com>
2026-07-06 13:35:54 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,9 @@
-const CACHE_NAME = "ala-cache-v1";
+const CACHE_NAME = "ala-cache-v2";
 
 const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icono.jpeg"
+  "./icono-192.jpg",
+  "./icono-512.jpg"
 ];
-
-// INSTALAR SERVICE WORKER
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// ACTIVAR
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
-// FETCH (modo offline)
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
